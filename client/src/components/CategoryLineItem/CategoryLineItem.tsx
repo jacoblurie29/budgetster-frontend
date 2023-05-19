79018b5904394f5e38ab5c28404b936a271b5b40
jacoblurie29 <jacoblurie29@gmail.com>
2023-05-18 21:44:47 -0400

    LargeCategoryCard completed and comments added

---
+++ client/src/components/CategoryLineItem/CategoryLineItem.tsx
@@ -0,0 +1,48 @@
+import "./CategoryLineItem.styles.css";
+/**
+ * @interface CategoryLineItemProps - Props for the CategoryLineItem component
+ *
+ * @prop {string} title - The title of the line item
+ * @prop {number} value - The dollar value of the line item
+ * @prop {number} percentage - The percentage of the total that the line item value represents
+ */
+interface CategoryLineItemProps {
+  title: string;
+  value: number;
+  percentage: number;
+}
+
+/**
+ * @component CategoryLineItem
+ * @description A line item in the LargeCategoryCard component.
+ * Displays the title, value, and percentage of the total that the value represents.
+ * Also displays a bar that represents the percentage of the total that the value represents.
+ */
+const CategoryLineItem = ({
+  title,
+  value,
+  percentage,
+}: CategoryLineItemProps) => {
+  return (
+    <div className="categorylineitem-container">
+      <div className="categorylineitem-lefthalf">
+        <div className="categorylineitem-title">{title}</div>
+        <div className="categorylineitem-value">
+          {value < 0 ? "-$" : "$"}
+          {value.toString().replace("-", "")}
+        </div>
+      </div>
+      <div className="categorylineitem-righthalf">
+        <div
+          className="categorylineitem-bar"
+          style={{
+            width: `${percentage}%`,
+          }}
+        />
+        <div className="categorylineitem-percentage">{percentage}%</div>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryLineItem;
--- client/src/components/LargeCategoryCard/LargeCategoryCard.tsx
+++ client/src/components/LargeCategoryCard/LargeCategoryCard.tsx
@@ -1,20 +1,46 @@
 import { MonetaryItem, TimePeriod } from "../../types/types";
-import CategoryBreakdownBar from "../CategoryBreakdownBar.tsx/CategoryBreakdownBar";
+import CategoryBreakdownBar from "../CategoryBreakdownBar/CategoryBreakdownBar";
+import CategoryLineItem from "../CategoryLineItem/CategoryLineItem";
+import "./LargeCategoryCard.styles.css";
 
+/**
+ * @interface LargeCategoryCardProps - Props for the LargeCategoryCard component
+ *
+ * @prop {string} title - The title of the card
+ * @prop {MonetaryItem[]} values - The values to display in the card
+ * @prop {TimePeriod} timePeriod - The time period that the values are represented in
+ */
 interface LargeCategoryCardProps {
   title: string;
   values: MonetaryItem[];
   timePeriod: TimePeriod;
 }
 
+/**
+ * @component LargeCategoryCard
+ * @description A card that displays a breakdown of a category's values.
+ * Displays a bar and information that represents the values in a monetary category.
+ * Top bar displays the percentage of the total that each value represents.
+ * Line items in the card display the title, value, and percentage of the total that each value represents.
+ */
 const LargeCategoryCard = ({
   title,
   values,
   timePeriod,
 }: LargeCategoryCardProps) => {
+  const total = values.reduce((a, b) => a + b.value, 0);
+
   return (
     <div className="largecategorycard-container">
       <CategoryBreakdownBar values={values} />
+      {values.map((value, index) => (
+        <CategoryLineItem
+          key={index}
+          title={value.name}
+          value={value.value}
+          percentage={Math.floor((value.value / total) * 100)}
+        />
+      ))}
     </div>
   );
 };
