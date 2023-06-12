4e90c680198b90f661a872d66940ba1ba6b501de
jacoblurie29 <jacoblurie29@gmail.com>
2023-06-12 11:35:45 -0500

    Sidebar styling

--- client/src/layout/sidebar/Sidebar.tsx
+++ client/src/layout/sidebar/Sidebar.tsx
@@ -1,8 +1,34 @@
 import { useState } from "react";
+import budgetsterIcon from "../../assets/BudgetsterIcon.png";
 import "./Sidebar.styles.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCog,
+  faHouse,
+  faMoneyBillTrendUp,
+  faPiggyBank,
+  faWallet,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
   const [sideBar, setSideBar] = useState<boolean>(false);
+  const [activeRoute, setActiveRoute] = useState<string>("Dashboard");
+
+  const sideBarTitles = [
+    "Dashboard",
+    "Expenses",
+    "Income",
+    "Investments",
+    "Settings",
+  ];
+
+  const sideBarIcons = [
+    faHouse,
+    faMoneyBillTrendUp,
+    faWallet,
+    faPiggyBank,
+    faCog,
+  ];
 
   return (
     <div
@@ -14,6 +40,51 @@ const Sidebar = () => {
         <button onClick={() => setSideBar(!sideBar)}>
           <i className={sideBar ? "arrow left" : "arrow right"}></i>
         </button>
+      </div>
+      <div
+        className={`sidebar-open ${
+          sideBar ? "sidebar-open-visible" : "sidebar-open-hidden"
+        }`}
+      >
+        <div className="sidebar-logo-container">
+          <img
+            className="sidebar-logo-image"
+            src={budgetsterIcon}
+            alt="budgster icon"
+          />
+        </div>
+        <div className={"sidebar-routes"}>
+          {sideBarTitles.map((title, index) => (
+            <div
+              className={
+                activeRoute === title
+                  ? "sidebar-route-active"
+                  : "sidebar-route-inactive"
+              }
+              key={index}
+            >
+              {title}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div
+        className={`sidebar-closed ${
+          sideBar ? "sidebar-closed-hidden" : "sidebar-closed-visible"
+        }`}
+      >
+        {sideBarTitles.map((title, index) => (
+          <div className="sidebar-closed-route" key={index}>
+            <FontAwesomeIcon
+              className={`sidebar-closed-icon ${
+                activeRoute === title
+                  ? "sidebar-closed-icon-active"
+                  : "sidebar-closed-icon-inactive"
+              }`}
+              icon={sideBarIcons[index]}
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
