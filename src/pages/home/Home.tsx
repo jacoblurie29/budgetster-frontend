0cf9ae53452dbcd785ef3150b80543ad021454d6
jacoblurie29 <jacoblurie29@gmail.com>
2023-07-30 14:30:18 -0500

    Homepage section two and scroll logic completed and responsive

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,8 @@
 import LandingTopBar from "../../components/LandingTopBar/LandingTopBar";
 import budgetsterLaptopPreview from "../../assets/BudgetsterLaptopPreview.png";
-import "./Home.styles.css";
+import budgetsterLaptopFront from "../../assets/BudgetsterLaptopFront.png";
+import "./Home.SectionOne.styles.css";
+import "./Home.SectionTwo.styles.css";
 
 const Home = () => {
   console.log("something");
@@ -32,6 +34,37 @@ const Home = () => {
             src={budgetsterLaptopPreview}
             alt=""
           />
+        </div>
+      </div>
+      <div className="home-screen-section-two">
+        <div className="home-screen-section-two-left">
+          <div className="home-screen-laptop-front-image-container">
+            <img
+              className="home-screen-laptop-front-image"
+              src={budgetsterLaptopFront}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="home-screen-section-two-right">
+          <div className="home-screen-section-two-list">
+            <div className="home-screen-section-two-list-item">
+              <span id="bullet-point-arrow">&#9658;</span>&nbsp;&nbsp;No cents,
+              ever.
+            </div>
+            <div className="home-screen-section-two-list-item">
+              <span id="bullet-point-arrow">&#9658;</span>&nbsp;&nbsp;No
+              complicated settings.
+            </div>
+            <div className="home-screen-section-two-list-item">
+              <span id="bullet-point-arrow">&#9658;</span>&nbsp;&nbsp;Set goals
+              and track your trends.
+            </div>
+            <div className="home-screen-section-two-list-item">
+              <span id="bullet-point-arrow">&#9658;</span>&nbsp;&nbsp;Easy time
+              frame toggling.
+            </div>
+          </div>
         </div>
       </div>
     </div>
